2f3ca194885acc7963740bfb78750b7e2fcb6cf0
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-09 21:01:23 -0300

    Builder and SLIME.

---
+++ prototypes/dungeon/dungeon/builder/hero.lua
@@ -0,0 +1,29 @@
+
+module ('dungeon.builder', package.seeall) do
+
+  require 'dungeon.entity'
+
+  function hero()
+    local hero = dungeon.entity:new {
+      image = love.graphics.newImage 'resources/avt1_fr1.gif',
+    }
+    function hero:input_pressed(button)
+      local next_pos = self.position:clone()
+      if button == 'right' then
+        next_pos.x = next_pos.x + 1
+      elseif button == 'left' then
+        next_pos.x = next_pos.x - 1
+      elseif button == 'up' then
+        next_pos.y = next_pos.y - 1
+      elseif button == 'down' then
+        next_pos.y = next_pos.y + 1
+      end
+      local next_tile = self.map:get_tile(next_pos)
+      if next_tile and next_tile.passable then
+        self.position = next_pos
+      end
+    end
+    return hero
+  end
+
+end
--- prototypes/dungeon/dungeon/loader.lua
+++ prototypes/dungeon/dungeon/loader.lua
@@ -4,6 +4,8 @@ module ('dungeon', package.seeall) do
   require 'dungeon.tile'
   require 'dungeon.mapscene'
   require 'dungeon.entity'
+  require 'dungeon.builder.hero'
+  require 'dungeon.builder.slime'
 
   function enter()
     local map = mapscene:new{ width = 15, height = 8 }
@@ -11,27 +13,11 @@ module ('dungeon', package.seeall) do
       map.matrix[j][7].passable = false
     end
 
-    local hero = entity:new {
-      image = love.graphics.newImage 'resources/avt1_fr1.gif',
-    }
-    function hero:input_pressed(button)
-      local next_pos = self.position:clone()
-      if button == 'right' then
-        next_pos.x = next_pos.x + 1
-      elseif button == 'left' then
-        next_pos.x = next_pos.x - 1
-      elseif button == 'up' then
-        next_pos.y = next_pos.y - 1
-      elseif button == 'down' then
-        next_pos.y = next_pos.y + 1
-      end
-      local next_tile = self.map:get_tile(next_pos)
-      if next_tile and next_tile.passable then
-        self.position = next_pos
-      end
-    end
+    local slime = builder.slime()
+    slime.position = vec2:new{12, 7}
 
-    map:add_entity(hero)
+    map:add_entity(builder.hero())
+    map:add_entity(slime)
 
     message.send [[main]] {'change_scene', map }
   end
