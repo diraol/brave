a99fc8010add970a516a4115386430ed446f5e30
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2013-10-23 22:09:03 -0200

    Atualizando o loader com a nova segmentação

--- prototypes/dungeon/dungeon/loader.lua
+++ prototypes/dungeon/dungeon/loader.lua
@@ -2,23 +2,26 @@
 module ('dungeon', package.seeall) do
 
   require 'dungeon.tile'
+  require 'dungeon.map'
   require 'dungeon.mapscene'
   require 'dungeon.entity'
   require 'dungeon.builder.hero'
   require 'dungeon.builder.slime'
 
   function enter()
-    local map = mapscene:new{ width = 15, height = 8 }
+    local map = map:new { width = 15, height = 8 }
     for j = 3,map.height do
       map.matrix[j][7].passable = false
     end
 
+    local dungeonscene = mapscene:new { map = map }
+
     local slime = builder.slime()
 
-    map:add_entity(builder.hero(),vec2:new{1,1})
-    map:add_entity(slime,vec2:new{12,7})
+    dungeonscene.timecontroller:add_entity(builder.hero(),vec2:new{1,1})
+    dungeonscene.timecontroller:add_entity(slime,vec2:new{12,7})
 
-    message.send [[main]] {'change_scene', map }
+    message.send [[main]] {'change_scene', dungeonscene }
   end
 
 end
