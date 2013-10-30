25369074620e84af8460b7b644b5bba699df8d65
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-30 17:46:01 -0200

    Adicionado scaling de entidades.

--- brave/dungeon/entity.lua
+++ brave/dungeon/entity.lua
@@ -6,6 +6,7 @@ module ('dungeon', package.seeall) do
   require 'dungeon.tile'
 
   entity = lux.object.new{
+    scale = 1.0,
     image = nil
   }
 
@@ -22,10 +23,11 @@ module ('dungeon', package.seeall) do
   function entity:draw(graphics)
     if self.image and self.position then
       local draw_pos = self.position * TILE_SIZE
-      draw_pos.y = draw_pos.y + TILE_SIZE - self.image:getHeight()
+      draw_pos.x = draw_pos.x + (TILE_SIZE - self.image:getWidth() * self.scale) * 0.5
+      draw_pos.y = draw_pos.y + TILE_SIZE - self.image:getHeight() * self.scale
 
       graphics.setColor(255, 255, 255)
-      graphics.draw(self.image, draw_pos:get())
+      graphics.draw(self.image, draw_pos.x, draw_pos.y, 0, self.scale, self.scale)
     end
   end
 end
