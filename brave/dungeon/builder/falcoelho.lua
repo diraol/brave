1ce0020278f817907d3a0341286db16d17ac213c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-10-30 17:42:31 -0200

    Caminho aleatório do falcoelho.

--- brave/dungeon/builder/falcoelho.lua
+++ brave/dungeon/builder/falcoelho.lua
@@ -6,7 +6,28 @@ module ('dungeon.builder', package.seeall) do
   function falcoelho(args)
     local falcoelho = dungeon.entity:new {
       image = love.graphics.newImage 'resources/entities/falcoelho.png',
+      scale = 0.5,
     }
+    function falcoelho:playturn()
+      local next_pos = self.position:clone()
+      local r = math.random()
+      if r < 0.25 then
+        next_pos.x = next_pos.x + 1
+      elseif r < 0.5 then
+        next_pos.x = next_pos.x - 1
+      elseif r < 0.75 then
+        next_pos.y = next_pos.y - 1
+      else
+        next_pos.y = next_pos.y + 1
+      end
+      local next_tile = self.map:get_tile(next_pos)
+      if next_tile and next_tile:available() then
+        self.map:get_tile(self.position):remove_entity()
+        self.map:get_tile(next_pos):add_entity(self)
+        self.position = next_pos
+      end
+      
+    end
     return falcoelho
   end
 
