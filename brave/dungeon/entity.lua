1e1c87a6371ef0573a4a9461774a96e29acdc22f
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2013-11-06 22:14:37 -0200

    Adicionada barra de vida e maxhp nas entidades

--- brave/dungeon/entity.lua
+++ brave/dungeon/entity.lua
@@ -8,11 +8,13 @@ module ('dungeon', package.seeall) do
   entity = lux.object.new{
     scale = 1.0,
     hp = 50,
+    maxhp = 50,
     image = nil
   }
 
   function entity:__init()
     self.position = self.position or vec2:new{1,1}
+    self.hp = self.maxhp
   end
 
   function entity:update(dt)
@@ -37,9 +39,18 @@ module ('dungeon', package.seeall) do
       local draw_pos = self.position * TILE_SIZE
       draw_pos.x = draw_pos.x + (TILE_SIZE - self.image:getWidth() * self.scale) * 0.5
       draw_pos.y = draw_pos.y + TILE_SIZE - self.image:getHeight() * self.scale
+      --calculando posição da barra de vida
+      local draw_pos_lifebar = self.position * TILE_SIZE
+      draw_pos_lifebar.x = draw_pos_lifebar.x
+      draw_pos_lifebar.y = draw_pos.y - 12
 
       graphics.setColor(255, 255, 255)
       graphics.draw(self.image, draw_pos.x, draw_pos.y, 0, self.scale, self.scale)
+      --Desenhando barra de vida
+      graphics.setColor(0, 255, 0)
+      graphics.rectangle("fill", draw_pos_lifebar.x, draw_pos_lifebar.y, TILE_SIZE * self.hp / 50, 8)
+      graphics.setColor(255, 255, 255)
+      graphics.rectangle("line", draw_pos_lifebar.x, draw_pos_lifebar.y, TILE_SIZE * self.maxhp / 50, 8)
     end
   end
 end
