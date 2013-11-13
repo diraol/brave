54ce46d9eb9430d75c8ac3c2a8ac827b5094f14c
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2013-11-13 21:41:04 -0200

    Adicionando o Pandarila ao projeto

--- brave/dungeon/levels/tiles.lua
+++ brave/dungeon/levels/tiles.lua
@@ -3,4 +3,5 @@ return {
 ["#"] = { type = 'tile', name = 'wall', blocks=true },
 ["F"] = { type = 'entity', name = 'falcoelho' },
 ["C"] = { type = 'entity', name = 'crocochorro' },
+["P"] = { type = 'entity', name = 'pandarila' },
 }
