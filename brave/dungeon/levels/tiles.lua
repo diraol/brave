84655867ca3ae4fa150b5d11c9b52b6b637e5336
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-11-13 19:52:36 -0200

    Adicionado tile 'empty'

--- brave/dungeon/levels/tiles.lua
+++ brave/dungeon/levels/tiles.lua
@@ -1,4 +1,5 @@
 return {
+[" "] = { type = 'tile', name = 'empty', blocks=true },
 ["."] = { type = 'tile', name = 'ground' },
 ["#"] = { type = 'tile', name = 'wall', blocks=true },
 ["F"] = { type = 'entity', name = 'falcoelho' },
