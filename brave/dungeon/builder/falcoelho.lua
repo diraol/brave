89f825cf0a4fcf769f8fca84775416b64ff20783
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2013-11-14 19:45:01 -0200

    Não repetindo o código de playturn igual

--- brave/dungeon/builder/falcoelho.lua
+++ brave/dungeon/builder/falcoelho.lua
@@ -13,39 +13,7 @@ return function(args)
     damage = 5,
     p_act = .2,
   }
-
-  function falcoelho:playturn()
-    if math.random() < self.p_act then return end
-
-    local wtd = math.random() -- What to do?
-
-    if wtd < self.p_attack_ratio then
-      if hero_distance(self.position, self.p_attack_distance) then
-        local local_scene = message.send [[main]] {'current_scene'}
-        hero_pos = local_scene.state.hero.position
-        local target_tile = self.map:get_tile(hero_pos)
-        if target_tile.entity then
-          target_tile.entity:take_damage(self.damage)
-        else
-          return
-        end
-      else
-        return
-      end
-    else
-      --do move to somewhere!
-      local next_pos = self.position:clone()
-      next_post = next_pos + random_side()
-
-      local next_tile = self.map:get_tile(next_pos)
-      if next_tile and next_tile:available() then
-        self.map:get_tile(self.position):remove_entity()
-        self.map:get_tile(next_pos):add_entity(self)
-        self.position = next_pos
-      end
-    end
-    
-  end
+  falcoelho.playturn = enemy_dumb_turn
 
   return falcoelho
 end
--- brave/dungeon/builder/util.lua
+++ brave/dungeon/builder/util.lua
@@ -26,3 +26,37 @@ function hero_distance(pos, range)
   hero_pos = local_scene.state.hero.position
   return hero_pos:distance(pos) <= range
 end
+
+function enemy_dumb_turn(self)
+  if math.random() < self.p_act then return end
+
+  local wtd = math.random() -- What to do?
+
+  if wtd < self.p_attack_ratio then
+    if hero_distance(self.position, self.p_attack_distance) then
+      local local_scene = message.send [[main]] {'current_scene'}
+      hero_pos = local_scene.state.hero.position
+      local target_tile = self.map:get_tile(hero_pos)
+      if target_tile.entity then
+        target_tile.entity:take_damage(self.damage)
+        return 0.1
+      else
+        return
+      end
+    else
+      return
+    end
+  else
+    --do move to somewhere!
+    local next_pos = self.position:clone()
+    next_post = next_pos + random_side()
+
+    local next_tile = self.map:get_tile(next_pos)
+    if next_tile and next_tile:available() then
+      self.map:get_tile(self.position):remove_entity()
+      self.map:get_tile(next_pos):add_entity(self)
+      self.position = next_pos
+      return 0.1
+    end
+  end
+end
